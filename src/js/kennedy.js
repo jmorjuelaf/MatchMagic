f103a57820cd274e6b981fd69ed8050ee1880245
jmorjuelaf <johannaof@hotmail.com>
2024-05-02 11:16:02 -0500

    correccion

--- src/js/kennedy.js
+++ src/js/kennedy.js
@@ -16,6 +16,7 @@ function drag(ev) {
     // En este caso, el dato es texto y el valor es el id del elemento arrastrado
     ev.dataTransfer.setData("text", ev.target.id);
 }
+
 // Qué sucede cuando se suelta un elemento arrastrado
 function drop(ev) {
     // Obtengo el nombre del id del div donde se soltará el elemento
@@ -35,10 +36,17 @@ function drop(ev) {
         ev.target.appendChild(lastDroppedElement);
         // Agregar una clase al contenedor original para indicar que la imagen ha sido devuelta
         newContainer.classList.add('dropped');
-            originalContainer = document.getElementById(`${data}-container`)
+        originalContainer = document.getElementById(`${data}-container`);
     }
 
-    // Función para mostrar SweetAlert con el mensaje de éxito o fracaso
+    // Controlo si ya están arrastrados todos los elementos
+    if (arreglo.every(element => element !== "")) {
+        // Controlo si hay coincidencia
+        const isSuccess = arreglo[0] === "americas" && arreglo[1] === "bavaria" && arreglo[2] === "calandaima" && arreglo[3] === "carvajal" && arreglo[4] === "castilla" && arreglo[5] === "corabastos" && arreglo[6] === "gran_britalia" && arreglo[7] === "kennedy_central" && arreglo[8] === "las_margaritas" && arreglo[9] === "patio_bonito" && arreglo[10] === "timiza" && arreglo[11] === "tintal_norte";
+        showResultAlert(isSuccess);
+    }
+}
+// Función para mostrar SweetAlert con el mensaje de éxito o fracaso
 function showResultAlert(isSuccess) {
     if (isSuccess) {
         Swal.fire({
@@ -63,16 +71,6 @@ function showResultAlert(isSuccess) {
     }
 }
 
-// Controlo si ya están arrastrados todos los elementos
-if (arreglo.every(element => element !== "")) {
-    // Controlo si hay coincidencia
-    const isSuccess = arreglo[0] === "americas" && arreglo[1] === "bavaria" && arreglo[2] === "calandaima" && arreglo[3] === "carvajal" && arreglo[4] === "castilla" && arreglo[5] === "corabastos" && arreglo[6] === "gran_britalia" && arreglo[7] === "kennedy_central" && arreglo[8] === "las_margaritas" && arreglo[9] === "patio_bonito" && arreglo[10] === "timiza" && arreglo[11] === "tintal_norte";
-    showResultAlert(isSuccess);
-}
-
-
-}
-
 // Deshacer el último movimiento
 function undoMove() {
     // Verificar si hay un elemento para deshacer y si tenemos el contenedor original
@@ -91,7 +89,6 @@ function undoMove() {
     }
 }
 
-
 // Restablecer el juego al estado inicial
 function resetGame() {
     // Reiniciar el arreglo de elementos arrastrados
@@ -100,20 +97,6 @@ function resetGame() {
     window.location.reload();
 }
 
-// Deshacer el último movimiento
-function undoMove() {
-    // Verificar si hay un elemento para deshacer y si tenemos el contenedor original
-    if (lastDroppedElement && originalContainer) {
-        // Devolver el elemento al contenedor original
-        originalContainer.appendChild(lastDroppedElement);
-        // Limpiar la entrada en el arreglo
-        let index = arreglo.indexOf(lastDroppedElement.id);
-        arreglo[index] = "", "", "", "", "", "", "", "", "", "", "", "";
-        // Limpiar la variable de último elemento soltado y el contenedor original
-        lastDroppedElement = null;
-        originalContainer = null;
-    }
-}
 // Función para redireccionar a index.html
 function goToIndex() {
     window.location.href = "index.html";
